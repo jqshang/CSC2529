656e3dd906f33cf374166f0b7d6ca345e982010a
Yifan Qu <quyf0516@outlook.com>
2025-11-14 19:19:14 -0500

    Refactor model names and process logic
    Updated model_names list and added processing logic for already processed cameras.

--- dataset/fivek_process.py
+++ dataset/fivek_process.py
@@ -27,12 +27,35 @@ args = parser.parse_args()
 raw_source_root = args.raw_source_root
 save_folder_root = args.save_folder_root
 
-
 # model_names = [
 #     "NIKON D70",
 #     "Canon EOS 5D",
 # ]
-model_names = ['Canon EOS 10D', 'Canon EOS 20D', 'Canon EOS 30D', 'Canon EOS 40D', 'Canon EOS 5D', 'Canon EOS 5D Mark II', 'Canon EOS D30', 'Canon EOS D60', 'Canon EOS DIGITAL REBEL', 'Canon EOS DIGITAL REBEL XSi', 'Canon EOS DIGITAL REBEL XT', 'Canon EOS DIGITAL REBEL XTi', 'Canon EOS-1D Mark II', 'Canon EOS-1D Mark II N', 'Canon EOS-1D Mark III', 'Canon EOS-1Ds Mark II', 'Canon PowerShot G10', 'Canon PowerShot G9', 'Canon PowerShot S70', 'D-LUX 3', 'DCS460D         FILE VERSION 3', 'DSLR-A900', 'FinePixS2Pro', 'M8 Digital Camera', 'NIKON D100', 'NIKON D200', 'NIKON D2H', 'NIKON D300', 'NIKON D40X', 'NIKON D70', 'NIKON D700', 'NIKON D70s', 'NIKON D80']
+
+model_names = ['Canon EOS 10D', 'Canon EOS 20D', 'Canon EOS 30D', 'Canon EOS 40D', 'Canon EOS 5D', 'Canon EOS 5D Mark II', 'Canon EOS D30', 'Canon EOS D60', 'Canon EOS DIGITAL REBEL XSi', 'Canon EOS DIGITAL REBEL XT', 'Canon EOS DIGITAL REBEL XTi', 'Canon EOS-1D Mark II', 'Canon EOS-1D Mark II N', 'Canon EOS-1D Mark III', 'Canon EOS-1Ds Mark II', 'Canon PowerShot G10', 'Canon PowerShot G9', 'Canon PowerShot S70', 'D-LUX 3', 'DCS460D         FILE VERSION 3', 'DSLR-A900', 'FinePixS2Pro', 'M8 Digital Camera', 'NIKON D100', 'NIKON D200', 'NIKON D2H', 'NIKON D300', 'NIKON D40X', 'NIKON D70', 'NIKON D700', 'NIKON D70s', 'NIKON D80']
+
+# Folder containing processed data
+processed_root = os.path.join(save_folder_root, "fivek_patches_3")
+processed_folders = [
+    f for f in os.listdir(processed_root)
+    if os.path.isdir(os.path.join(processed_root, f))
+]
+
+# Convert model names to folder-like strings
+model_names_folder = [name.replace(" ", "_") for name in model_names]
+
+# Remove already processed cameras
+for folder in processed_folders:
+    # Remove _train_raw or _test_raw suffix
+    folder_clean = folder.replace("_train_raw", "").replace("_test_raw", "")
+    if folder_clean in model_names_folder:
+        idx = model_names_folder.index(folder_clean)
+        # Remove both the folder-style and the original model name
+        model_names_folder.pop(idx)
+        model_names.pop(idx)
+
+print("Remaining models to process:", model_names)
+
 
 raw_file_extensions = [".dng"]
 
